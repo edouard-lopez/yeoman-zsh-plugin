ed7ce5e135b160c738eabf65da194f56f8d487f9
Sayanee <sayanee@gmail.com>
2013-01-20 19:23:35 +0800

    initial commit with basic yeoman alias and zsh autocomplete

---
+++ yeoman.plugin.zsh
@@ -0,0 +1,18 @@
+alias ymget='curl -L get.yeoman.io | bash'
+
+alias ym='yeoman help'
+alias ymv='yeoman --version'
+
+alias ymi='yeoman init'
+alias ymb='yeoman build'
+alias ymsv='yeoman server'
+alias ymsd='yeoman server:dist'
+alias ymst='yeoman server:test'
+
+alias ymt='yeoman test'
+alias ymii='yeoman install'
+alias ymui='yeoman uninstall'
+alias ymu='yeoman update'
+alias yml='yeoman list'
+alias yms='yeoman search'
+alias ymlu='yeoman lookup'
